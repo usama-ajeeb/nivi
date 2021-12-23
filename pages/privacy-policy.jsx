9509f78b07f812d38c17c91ea206bbde75448b35
HarisJsDev <usmaniseo@gmail.com>
2021-12-23 20:15:36 +0500

    4rth

---
+++ pages/privacy-policy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GeneralHeader from '../components/GeneralHeader'
+
+function privacyPolicy() {
+  return (
+    <div>
+      <GeneralHeader />
+    </div>
+  )
+}
+
+export default privacyPolicy
